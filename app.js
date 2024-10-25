7c8996f82e11853291b288f07a3a6c8d99881d49
Nahom.T <ntade499@gmail.com>
2024-10-25 11:37:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const axios = require("axios"); // Add axios to the require statements
+const { testConnection } = require("./db/dbConfig"); // Import testConnection
 const app = express();
 const cors = require("cors");
 app.use(cors());
 
-// db connection
-const dbConnection = require("./db/dbConfig");
+app.use(express.json());
 
 // user routes middleware
 const userRoutes = require("./routes/userRoute");
@@ -15,31 +15,23 @@ const answerRoute = require("./routes/answerRoute");
 // authentication middleware
 const authMiddleware = require("./middleware/authMiddleware");
 
-app.use(express.json());
-
-// user routes
 app.use("/api/user", userRoutes);
-
-// questions routes
 app.use("/api/questions", authMiddleware, questionsRoute);
-
-// answers routes
 app.use("/api/answer", authMiddleware, answerRoute);
 
 async function start() {
   try {
-    const result = await dbConnection.execute("SELECT 'Test'");
-    console.log(result);
+    // Test the database connection
+    const result = await testConnection();
+    console.log("Database connected successfully:", result);
 
     // Fetch and log the Render server's public IP address
     const ipResponse = await axios.get("https://api64.ipify.org?format=json");
     console.log("Render Server IP Address:", ipResponse.data.ip);
 
-    // Use dynamic port selection (if 5555 is busy, another free port will be used)
-    const PORT = process.env.PORT || 0;  
+    const PORT = process.env.PORT || 3000;  
     const server = app.listen(PORT, () => {
-      console.log("Database connection established");
-      console.log(`Listening on ${server.address().port}`);
+      console.log(`Listening on port ${server.address().port}`);
     });
 
     // Handle server-level errors
